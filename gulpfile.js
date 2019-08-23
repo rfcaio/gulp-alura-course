670d38263b45d2702a6994ea7e13d80fddd68f26
Caio Rodrigues <rfcaio90@gmail.com>
2019-08-23 15:06:03 -0300

    gulp-aula-03

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-var gulp = require('gulp');
 var del = require('del');
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var htmlReplace = require('gulp-html-replace');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 /* jshint strict: false */
 
@@ -13,6 +18,41 @@ var copy = function copy () {
     .pipe(gulp.dest('dist'));
 };
 
+var html = function html () {
+  return gulp
+    .src('src/index.html')
+    .pipe(htmlReplace({
+      css: 'style.css',
+      js: 'bundle.js'
+    }))
+    .pipe(gulp.dest('dist'));
+};
+
+var scripts = function scripts () {
+  return gulp
+    .src(['node_modules/angular/angular.js', 'src/js/app.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+};
+
+var styles = function styles () {
+  return gulp
+    .src(['node_modules/bootstrap/dist/css/bootstrap.css', 'src/css/main.css'])
+    .pipe(concat('style.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
+};
+
+var build = gulp.series(clean, gulp.parallel(html, scripts, styles));
+
+exports.build = build;
 exports.clean = clean;
 exports.copy = copy;
-exports.default = gulp.series(clean, copy);
+exports.html = html;
+exports.scripts = scripts;
+exports.styles = styles;
+
+exports.default = build;
